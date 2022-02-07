14f39e1e037c54cfad739d67e3bd569cc12ebd0f
Utku Eray <m.utkueray@gmail.com>
2022-02-07 23:03:49 +0300

    implemented firebase structure

--- TeaDoAPI/django_tea_do/django_tea_do/urls.py
+++ TeaDoAPI/django_tea_do/django_tea_do/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from tea_do import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('api/v1/teado/',include("tea_do.urls"))
 ]
--- TeaDoAPI/django_tea_do/tea_do/views.py
+++ TeaDoAPI/django_tea_do/tea_do/views.py
@@ -5,8 +5,36 @@ from rest_framework.generics import DestroyAPIView
 from rest_framework.generics import UpdateAPIView
 from tea_do.serializers import TeaDoSerializer
 from tea_do.models import TeaDo
+import pyrebase
 
-# Create your views here.
+config = {
+    "apiKey": "AIzaSyAq2B7WmmxTlyUuQIk9aC9XzPirxdBXNRI",
+    "authDomain": "teado-e6cb4.firebaseapp.com",
+    "projectId": "teado-e6cb4",
+    "storageBucket": "teado-e6cb4.appspot.com",
+    "messagingSenderId": "948596075451",
+    "appId": "1:948596075451:web:1672b8b8a17bbe1f62b29e",
+    "measurementId": "G-SE1SPN9FXM",
+    "databaseURL": "https://teado-e6cb4-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+firebase = pyrebase.initialize_app(config)
+authe = firebase.auth()
+database = firebase.database()
+
+def index(request):
+        #accessing our firebase data and storing it in a variable
+        name = database.child('Data').child('Name').get().val()
+        stack = database.child('Data').child('Stack').get().val()
+        framework = database.child('Data').child('Framework').get().val()
+    
+        context = {
+            'name':name,
+            'stack':stack,
+            'framework':framework
+        }
+        return render(request, 'index.html', context)
+
 class ListAPIView(ListAPIView):
     """This endpoint list all of the available todos from the database"""
     queryset = TeaDo.objects.all()
